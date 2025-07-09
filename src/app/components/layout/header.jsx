cd9fbf83b483c71821e90e004bbafd9178cc05f2
sayhi99 <tpghk630@naver.com>
2025-07-09 18:05:44 +0900

    풀페이지 기능 추가, 헤더 기능추가

--- src/app/components/layout/header.jsx
+++ src/app/components/layout/header.jsx
@@ -1,97 +1,52 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import AOS from 'aos';
 import Link from 'next/link';
-import fullpage from 'fullpage.js';
-import 'fullpage.js/dist/fullpage.css';
 import './header.css';
 
 export default function Header() {
-  // useEffect로 초기화
+  const [mnavOpen, mnavClose] = useState(false);
+
+  const handleBurgerClick = () => {
+    mnavClose(!mnavOpen);
+  };
+
+  const handleMnavClick = (e) => {
+    e.stropPropagation();
+    setMnavOpen(false);
+  };
+
+  // AOS 초기화
   useEffect(() => {
     AOS.init({
       duration: 1000,
     });
-    if (typeof window !== 'undefined' && window.fullpage) {
-      window.fullpage('#fullpage', {
-        licenseKey: '8KZNK-N2PA6-HR1W6-1JLGI-EYBNL',
-        //기본 설정
+  }, []);
 
-        scrollingSpeed: 1000,
-        scrollOverflow: true,
-        scrollOverflowReset: true,
-        scrollOverflowOptions: {
-          click: true,
-          scrollbars: true,
-          mouseWheel: true,
-          interactiveScrollbars: true,
-          shrinkScrollbars: 'scale',
-          fadeScrollbars: true,
-        },
-        normalScrollElements: '.scroll_section',
-        //네비게이션
-        navigation: false,
-        slidesNavigation: true,
-        menu: true,
-        anchors: [
-          '1page',
-          '2page',
-          '3page',
-          '4page',
-          '5page',
-          '6page',
-          '7page',
-        ],
+  //모바일네비 버거버튼 클릭
+  useEffect(() => {
+    const burger = document.querySelector('.burger');
+    const mnavWrap = document.querySelector('.mnav_wrap');
 
-        //콜백 추가
-        afterLoad: function (anchorLink, index) {
-          headerNav(index.index);
-          if (typeof window.headerNav === 'function') {
-            window.headerNav(index.index);
-          }
-
-          if (index.index === 0 || index.index === 7) {
-            $('.dial').removeClass('on');
-            const dialElements = document.querySelectorAll('.dial');
-            dialElements.forEach((el) => el.classList.remove('on'));
-          } else {
-            const dialElements = document.querySelectorAll('.dial');
-            dialElements.forEach((el) => el.classList.add('on'));
-            // 섹션 인덱스에 따라 dial 회전
-            if (typeof window.updateTransforms === 'function') {
-              window.updateTransforms(index.index);
-            }
-          }
-
-          if (index.index === 2) {
-            const countingElements = document.querySelectorAll('.counting');
-            countingElements.forEach((element) => {
-              const countTo = element.getAttribute('data-count');
-              let currentCount = 0;
-              const duration = 1500;
-              const increment = countTo / (duration / 16);
-              const $this = $(element);
-
-              const animate = function () {
-                currentCount += increment;
-                $this.text(Math.floor(currentCount).toLocaleString());
-                if (currentCount < countTo) {
-                  element.textContent =
-                    Math.floor(currentCount).toLocaleString();
-                  requestAnimationFrame(animate);
-                } else {
-                  element.textContent = countTo.toLocaleString();
-                }
-              };
-              animate();
-            });
-          }
-        },
-        //반응형
-        responsiveWidth: 1280,
-        // fixedElements: 'header',
-      });
+    if (burger && mnavWrap) {
+      // 클래스 토글
+      if (mnavOpen) {
+        burger.classList.add('on');
+        mnavWrap.classList.add('on');
+      } else {
+        burger.classList.remove('on');
+        mnavWrap.classList.remove('on');
+      }
     }
+  }, [mnavOpen]);
+
+  //모바일네비 메뉴 클릭
+  useEffect(() => {
+    $('.mnav_menu > a').on('click', function (e) {
+      e.stopPropagation(); // 이벤트 버블링 방지
+      $('.mnav_wrap').removeClass('on');
+      $('.global_header .burger').removeClass('on');
+    });
   }, []);
   return (
     <>
@@ -127,31 +82,49 @@ export default function Header() {
               <Link href={'#7page'}>Contact</Link>
             </li>
           </ul>
-          <button className="burger">
+          <button
+            className={`burger ${mnavOpen ? 'on' : ''}`}
+            onClick={handleBurgerClick}
+          >
             <span></span>
             <span></span>
             <div className="blind">메뉴 열기/닫기</div>
           </button>
-          <div className="mnav_wrap" data-lenis-prevent>
+          <div
+            className={`mnav_wrap ${mnavOpen ? 'on' : ''}`}
+            data-lenis-prevent
+          >
             <div className="inner">
               <ul className="mnav">
                 <li className="mnav_menu">
-                  <Link href={'#2page'}>Director</Link>
+                  <Link href={'#2page'} onClick={handleMnavClick}>
+                    Director
+                  </Link>
                 </li>
                 <li className="mnav_menu">
-                  <Link href={'#3page'}>About</Link>
+                  <Link href={'#3page'} onClick={handleMnavClick}>
+                    About
+                  </Link>
                 </li>
                 <li className="mnav_menu">
-                  <Link href={'#4page'}>Philosophy</Link>
+                  <Link href={'#4page'} onClick={handleMnavClick}>
+                    Philosophy
+                  </Link>
                 </li>
                 <li className="mnav_menu">
-                  <Link href={'#5page'}>Services</Link>
+                  <Link href={'#5page'} onClick={handleMnavClick}>
+                    Services
+                  </Link>
                 </li>
                 <li className="mnav_menu">
-                  <Link href={'#6page'}>Portfolio</Link>
+                  <Link href={'#6page'} onClick={handleMnavClick}>
+                    Portfolio
+                  </Link>
                 </li>
                 <li className="mnav_menu">
-                  <Link href={'#7page'}>Contact</Link>
+                  <Link href={'#7page'} onClick={handleMnavClick}>
+                    Contact
+                  </Link>
                 </li>
               </ul>
             </div>
